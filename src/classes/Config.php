BeOleg/PHPcontacts

commit fa541a4e66ac1e118788bfb1b83b6a4be87083d4

Set SELF to lower case in all of the classes

src/classes/Router.php
@@ -25,26 +25,26 @@ class Router{
 	);
 	private static $ACTIVE_CLASS = "active";
 	public static function includeView($param){
-		if(isset(SELF::$VIEW_LOOKUP[$param]) && SELF::$VIEW_LOOKUP[$param]){
-			require_once($_SERVER['DOCUMENT_ROOT'] . SELF::$VIEW_LOOKUP[$param]);
+		if(isset(self::$VIEW_LOOKUP[$param]) && self::$VIEW_LOOKUP[$param]){
+			require_once($_SERVER['DOCUMENT_ROOT'] . self::$VIEW_LOOKUP[$param]);
 			return;
 		}
 	
 	}
 	public static function includeCtrl($param){
 		if($param === null){
-			require_once($_SERVER['DOCUMENT_ROOT'] . SELF::$CTRL_LOOKUP['main']);
+			require_once($_SERVER['DOCUMENT_ROOT'] . SEL:F:$CTRL_LOOKUP['main']);
 			return;
 		}
-		if(isset(SELF::$CTRL_LOOKUP[$param]) && SELF::$CTRL_LOOKUP[$param]){
-			require_once($_SERVER['DOCUMENT_ROOT'] . SELF::$CTRL_LOOKUP[$param]);
+		if(isset(self::$CTRL_LOOKUP[$param]) && self::$CTRL_LOOKUP[$param]){
+			require_once($_SERVER['DOCUMENT_ROOT'] . self::$CTRL_LOOKUP[$param]);
 			return;
 		}
-		require_once($_SERVER['DOCUMENT_ROOT'] . SELF::$CTRL_LOOKUP['main']);//404, redirect to main page
+		require_once($_SERVER['DOCUMENT_ROOT'] . self::$CTRL_LOOKUP['main']);//404, redirect to main page
 	}
 	public static function isActivePath($param){
 		$currPath = $_SERVER['REQUEST_URI'];		
-		return strpos($currPath, $param) ? SELF::$ACTIVE_CLASS : '';
+		return strpos($currPath, $param) ? self::$ACTIVE_CLASS : '';
 	}
 	public static function getLink($route){
 		$currPath = $_SERVER['REQUEST_URI'];
@@ -64,10 +64,10 @@ class Router{
 	}
 	public static function getStatics($route, $type){
 		$route = $route == '' ? 'main' : $route;
-		$specific_statics = isset(SELF::$PAGE_SPECIFIC_STATICS[$route]) && isset(SELF::$PAGE_SPECIFIC_STATICS[$route][$type]) && sizeof(SELF::$PAGE_SPECIFIC_STATICS[$route][$type]) > 0 ?
-								 SELF::$PAGE_SPECIFIC_STATICS[$route][$type]  : array();
-		$global_statics = isset(SELF::$GLOBAL_STATICS[$type]) && sizeof(SELF::$GLOBAL_STATICS[$type]) > 0 ?
-								 SELF::$GLOBAL_STATICS[$type] : array();
+		$specific_statics = isset(self::$PAGE_SPECIFIC_STATICS[$route]) && isset(self::$PAGE_SPECIFIC_STATICS[$route][$type]) && sizeof(self::$PAGE_SPECIFIC_STATICS[$route][$type]) > 0 ?
+								 self::$PAGE_SPECIFIC_STATICS[$route][$type]  : array();
+		$global_statics = isset(self::$GLOBAL_STATICS[$type]) && sizeof(self::$GLOBAL_STATICS[$type]) > 0 ?
+								 self::$GLOBAL_STATICS[$type] : array();
 		return array_merge($global_statics, $specific_statics);
 	}
 	public static function showSearchDialog($viewValue){
@@ -75,7 +75,7 @@ class Router{
 		return $boolFlag;
 	}
 	public static function render($ctrl, $data){
-		$views = SELF::ctrlToView($ctrl);
+		$views = self::ctrlToView($ctrl);
 		extract($data);
 		require($_SERVER['DOCUMENT_ROOT'] . $views['header']);
 		foreach($list as $__item){
@@ -83,7 +83,7 @@ class Router{
 		}
 	}
 	public static function RenderEmptyResultSet($ctrl){
-		$views = SELF::ctrlToView($ctrl);
+		$views = self::ctrlToView($ctrl);
 		require_once($_SERVER['DOCUMENT_ROOT'] . $views['header']);
 		$phrase = Dictionary::dictLookup('NO_RESULTS');
 		$msg = "<h4>{$phrase}</h4>";

src/classes/DB.php
@@ -3,7 +3,7 @@
 		public static function getFullName($userId){
 			$sql = "SELECT CONCAT(first_name, ' ', last_name) as fullName from users where id = :uid";
 			try{
-				$conn = SELF::getInstance();
+				$conn = self::getInstance();
 				$sth = $conn->prepare($sql);
 				$sth->bindParam(':uid', $userId, PDO::PARAM_INT);
 				$sth->execute();
@@ -33,7 +33,7 @@
 					
 					}
 					try{
-						$conn = SELF::getInstance();
+						$conn = self::getInstance();
 						$sth = $conn->prepare($sql);
 						if($queryString){
 							$sth->bindValue(':queryString', "%{$queryString}%");
@@ -59,7 +59,7 @@
 				$sql .= ' AND (CONCAT(first_name, " ", last_name) LIKE :queryString OR CONCAT(last_name, " ", first_name) LIKE :queryString)';
 			}			
 			try{
-				$conn = SELF::getInstance();
+				$conn = self::getInstance();
 				$sth = $conn->prepare($sql);
 				$sth->bindParam(':uid', $userId, PDO::PARAM_INT);
 				if($queryString){
@@ -80,7 +80,7 @@
 				$sql .= ' WHERE (CONCAT(u.first_name, " ", u.last_name) LIKE :queryString OR CONCAT(u.last_name, " ", u.first_name) LIKE :queryString)';
 			}
 			try{
-				$conn = SELF::getInstance();
+				$conn = self::getInstance();
 				$sth = $conn->prepare($sql);
 				if($queryString){
 					$sth->bindValue(':queryString', "%{$queryString}%");
@@ -98,7 +98,7 @@
 		          $fieldnames = isset($data[0]) ? array_keys($data[0]) : array_keys($data);
 		          $count_inserts = count(array_values($data));
 		          $count_values = isset($data[0]) ? count(array_values($data[0])) : count(array_values($data));
-		          $conn = SELF::getInstance();
+		          $conn = self::getInstance();
 
 		          for($i = 0; $i < $count_values; $i++)
 		          {
@@ -156,7 +156,7 @@
 		}
 		public function exec($sql){
 			try{
-				$conn = SELF::getInstance();
+				$conn = self::getInstance();
 				$conn->exec($sql);
 			}
 		   catch(PDOException $e){

src/models/population.php
@@ -52,11 +52,11 @@
 			);
 
 		public function __construct(){
-				foreach(SELF::$tables as $table){
+				foreach(self::$tables as $table){
 					DB::exec($table);
 				}
-				DB::multipleInsert('users', SELF::$users);
-				DB::multipleInsert('contacts', SELF::$contacts);
+				DB::multipleInsert('users', self::$users);
+				DB::multipleInsert('contacts', self::$contacts);
 		}
 	}
 

commit a3a033e5161cbea1e16acabfdd33bad25c598cee

Fixed parser error caused by fa541a4e66ac1e118788bfb1b83b6a4be87083d4

src/classes/Config.php
@@ -1,10 +1,0 @@
-<? 
-	class config{
-		public static $DB_SERVER = 'localhost';	
-		public static $DB_NAME = 'task';
-		public static $DB_USER = 'root';//not recommended to use root in prod
-		public static $DB_PASSWORD = 'devdev!@#!@##@!';
-		public static $DEBUG_MODE = true;
-		public static $DEFAULT_BDAY_RANGE = 3;
-	}
-?>

src/classes/Router.php
@@ -33,7 +33,7 @@ class Router{
 	}
 	public static function includeCtrl($param){
 		if($param === null){
-			require_once($_SERVER['DOCUMENT_ROOT'] . self:$CTRL_LOOKUP['main']);
+			require_once($_SERVER['DOCUMENT_ROOT'] . self::$CTRL_LOOKUP['main']);
 			return;
 		}
 		if(isset(self::$CTRL_LOOKUP[$param]) && self::$CTRL_LOOKUP[$param]){