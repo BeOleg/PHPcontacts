d4a3d64cb0eba1bbe490ab0dd80ff306da1393ab
Oleg Belousov <olegb@tomigo.com>
2014-01-24 03:13:53 +0200

    Return actuak 400 status from API on bad request

--- api.php
+++ api.php
@@ -1,9 +1,18 @@
 <?php
   require_once($_SERVER['DOCUMENT_ROOT'] . '/src/includes.php');
-  if(!isset($_GET['endpoint']) || !$_GET['endpoint'])
-  	 throw new exception('400');//Raise 400 error, bad request
+  if(!isset($_GET['endpoint']) || !$_GET['endpoint']){
+    http_response_code(400); 
+    throw new exception('Status code: 400');//Raise 400 error, bad request
+  }
+  	 
   $serializer = SerializerFactory::getSerializer($_GET['endpoint']);
-  $data = $serializer->getData($_GET);//get is actually supoer global, but nvm that, it is probper to pass the request as parameter to the serializer
+  if($serializer && is_callable(array($serializer, 'getData'))){ //TODO: create abstract serializer class and to check that $serializer is instanceOf it
+    $data = $serializer->getData($_GET);//get is actually supoer global, but nvm that, it is probper to pass the request as parameter to the serializer
+  }
+  else{
+    http_response_code(400);
+    throw new exception('Status code:  400');//raid 400, bad request, TODO: catch this exception, or replace it with exit()
+  }
 
   if(isset($data) && isset($data['list']) && sizeof($data['list']) > 0){
   	Router::Render($_GET['endpoint'], $data);	
