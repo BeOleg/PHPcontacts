1b8f88c07d8f742036ce433c33841b06ba4f79f3
Oleg Belousov <olegb@tomigo.com>
2014-01-24 16:28:01 +0200

    Fixed DB singleton structure

--- src/classes/DBLink.php
+++ src/classes/DBLink.php
@@ -7,18 +7,17 @@ abstract class DBLink{
     }
 
     public static function getInstance() {
-        if(isset(self::$conn)){
+        if(isset(self::$conn) && self::$conn){
           return self::$conn;
         }
         else{
             try{
-//echo config::$DB_PASSWORD;
-                $conn = new PDO('mysql:host='.config::$DB_SERVER.';dbname='. config::$DB_NAME . ';charset=UTF8', config::$DB_USER, config::$DB_PASSWORD, array(PDO::ATTR_PERSISTENT => true));//Use persistent connection, since the search mechanism causes numerious concurrent and subsequent requests to the db
-                $conn->exec("SET NAMES utf8"); // Tell the db that we use utf-8 
-                $conn->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true); //for heavy traffic optimization
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                // $conn-> setAttribute(PDO::ATTR_PERSISTENT, true);
-                return $conn;
+                self::$conn = new PDO('mysql:host='.config::$DB_SERVER.';dbname='. config::$DB_NAME . ';charset=UTF8', config::$DB_USER, config::$DB_PASSWORD, array(PDO::ATTR_PERSISTENT => true));//Use persistent connection, since the search mechanism causes numerious concurrent and subsequent requests to the db
+                self::$conn->exec("SET NAMES utf8"); // Tell the db that we use utf-8 
+                self::$conn->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true); //for heavy traffic optimization
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$conn->setAttribute(PDO::ATTR_PERSISTENT, true);//Use persistent connection, since the search mechanism causes numerious concurrent and subsequent requests to the db
+                return self::$conn;
              }
             catch (PDOException $e) {
                 Debugger::HandleException($e);
